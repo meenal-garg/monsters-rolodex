13b45cdf35b3757321a34e95750553ba90e24184
meenal-garg <meenalgarg990@gmail.com>
2020-03-04 18:38:08 +0530

    Updates

---
+++ precache-manifest.3608efc90dfccc8f175304f4b6dfd33f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3690ecbcfb6057398ed2347c77feb02",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3ec44d20ee4e13c5fc83",
+    "url": "/monsters-rolodex/static/css/main.68328606.chunk.css"
+  },
+  {
+    "revision": "3db869bb7de2277be351",
+    "url": "/monsters-rolodex/static/js/2.e603f8d1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.e603f8d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec44d20ee4e13c5fc83",
+    "url": "/monsters-rolodex/static/js/main.d88d8ccd.chunk.js"
+  },
+  {
+    "revision": "c974309c68a836970a36",
+    "url": "/monsters-rolodex/static/js/runtime-main.68be9c1c.js"
+  }
+]);
